bee770e7aff241d21003f7a2eb7517db9c49f9e3
Attila Horvath <sweeptic@gmail.com>
2025-03-03 15:33:17 +0100

    009 Adjusting Server Actions for useFormState

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,10 +1,10 @@
-import FormSubmit from '@/components/form-submit';
+import PostForm from '@/components/post-form';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
-import { useFormStatus } from 'react-dom';
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  // this is server action
+  async function createPost(prevState, formData) {
     'use server';
     const title = formData.get('title');
     const image = formData.get('image');
@@ -20,11 +20,12 @@ export default function NewPostPage() {
       errors.push('content is required');
     }
 
-    if (!image) {
+    if (!image && image.size === 0) {
       errors.push('image is required');
     }
 
-    if (ErrorEvent.length > 0) {
+    if (errors.length > 0) {
+      return { errors };
     }
 
     storePost({
@@ -34,31 +35,8 @@ export default function NewPostPage() {
       userId: 1,
     });
 
-    redirect('/feed');
+    // redirect('/feed');
   }
 
-  const [state, formAction] = useFormStatus(createPost, {});
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input type="file" accept="image/png, image/jpeg" id="image" name="image" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
