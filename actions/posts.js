d6fbdd3b12ada974b714e59a040bc6a0ef9c9ced
Attila Horvath <sweeptic@gmail.com>
2025-03-03 15:49:03 +0100

    010 Storing Server Actions In Separate Files

---
+++ actions/posts.js
@@ -0,0 +1,38 @@
+'use server';
+
+import { storePost } from '@/lib/posts';
+import { redirect } from 'next/navigation';
+
+export async function createPost(prevState, formData) {
+  'use server';
+  const title = formData.get('title');
+  const image = formData.get('image');
+  const content = formData.get('content');
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push('title is required');
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push('content is required');
+  }
+
+  if (!image && image.size === 0) {
+    errors.push('image is required');
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  storePost({
+    imageUrl: '',
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect('/feed');
+}
